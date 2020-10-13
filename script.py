27eacb967cca57c3d172465c9deb36254c230c0c
Official21A <officialamirhossein21@gmail.com>
2020-10-13 07:52:08 +0330

    Def create

--- script.py
+++ script.py
@@ -1,6 +1,12 @@
+import random
 
 total_len = 16
 input_list = []
+upper_letters = [chr(i) for i in range(65, 91)]
+lower_letters = [chr(i) for i in range(97, 123)]
+digits = [str(i) for i in range(0, 10)]
+
+
 
 while True:
 	string = input("Enter >> ")
@@ -8,5 +14,8 @@ while True:
 		break
 	input_list.append(string)
 
+def id_generator(size=6, chars=upper_letters+lower_letters+digits+input_list):
+    return ''.join(random.choice(chars) for i in range(size))
 
-print(input_list)	
+
+print(id_generator(size=total_len))	
