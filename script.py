c53c3ff7e663c6929fa8bc5e13a3136e86d4238b
Official21A <officialamirhossein21@gmail.com>
2020-10-13 08:05:25 +0330

    Refactor

--- script.py
+++ script.py
@@ -1,25 +1,19 @@
 import random
 
-total_len = 16
 input_list = []
 upper_letters = [chr(i) for i in range(65, 91)]
 lower_letters = [chr(i) for i in range(97, 123)]
 digits = [str(i) for i in range(0, 10)]
 
 
+def password_get(size=10, chars=upper_letters+lower_letters+digits):
+	global input_listm
 
-while True:
-	string = input("Enter >> ")
-	if string == "done()":
-		break
-	input_list.append(string)
-
-def id_generator(size=6, chars=upper_letters+lower_letters+digits):
-	global input_list
 	password = []
+
 	while size:
 		stat = random.randint(0, 200)
-		if stat % 3 == 0:
+		if stat % 3 == 0 and input_list:
 			string = random.choice(input_list)
 			if not string in password:
 				password.append(string)
@@ -32,4 +26,16 @@ def id_generator(size=6, chars=upper_letters+lower_letters+digits):
 	return ''.join(password)
 
 
-print(id_generator(size=total_len))	
+def main():
+	global input_list
+	while True:
+		string = input("Enter >> ")
+		if string == "done()":
+			break
+		input_list.append(string)
+
+	print(password_get())	
+
+
+if __name__ == "__main__":
+	main()	
